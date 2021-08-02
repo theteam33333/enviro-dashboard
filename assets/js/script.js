f0dedca3b6456a372ae0eb1599876f9c743ad369
Craig Martin <85413293+Cragar777@users.noreply.github.com>
2021-08-02 17:22:23 -0400

    implemented the guardian api with ability to select an article on climate change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
+// fetch ("https://api.climateclock.world/v1/clock") 
+fetch ("https://content.guardianapis.com/search?q=climate%20change&api-key=2b864c12-3fa4-4b07-a5fa-72ff409c8dc3")
 
+  .then (function(res) {
+    return res.json();
+  })
+
+  .then (function(data) {
+    console.log(data);
+    var climateClockEl = document.querySelector('.climateClock');
+    var webTitle = (data.response.results[0].webTitle);
+    var webUrl = (data.response.results[0].webUrl);
+    var buttonEl = document.createElement("button");
+    var aEl = document.createElement("a");
+    aEl.href=webUrl;
+    aEl.innerHTML=webTitle;
+    console.log(aEl);
+    //buttonEl.textContent = aEl;
+    climateClockEl.appendChild(aEl);
+  });
 
 
 

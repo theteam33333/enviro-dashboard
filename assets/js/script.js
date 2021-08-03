760c55212741d62bc3b718f7114bc784afbc974a
SINGH 1258 <sumantpaldm@gmail.com>
2021-08-03 01:51:08 -0400

    updated files with main branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //quiz code start
 let containerEl = document.querySelector("#container");
 let headerEl = document.querySelector(".header");
@@ -57,7 +56,7 @@ const greenEmission = [
   },
 ];
 
-const ozone = [
+const ozone1 = [
   {
     question: "What do chlorofluorocarbons (CFCs) do?",
     choices: {
@@ -267,7 +266,7 @@ let random = [];
 
 
 random.push(greenEmission[(Math.floor(Math.random() * 4))])
-random.push(ozone[(Math.floor(Math.random() * 4))])
+random.push(ozone1[(Math.floor(Math.random() * 4))])
 random.push(global[(Math.floor(Math.random() * 4))])
 random.push(carbonFoot[(Math.floor(Math.random() * 4))])
 random.push(deforestation[(Math.floor(Math.random() * 4))])
@@ -299,29 +298,27 @@ function displayQuestions() {
         `
     );
   }
-=======
-// fetch ("https://api.climateclock.world/v1/clock") 
-fetch ("https://content.guardianapis.com/search?q=climate%20change&api-key=2b864c12-3fa4-4b07-a5fa-72ff409c8dc3")
+  // fetch ("https://api.climateclock.world/v1/clock") 
+  fetch("https://content.guardianapis.com/search?q=climate%20change&api-key=2b864c12-3fa4-4b07-a5fa-72ff409c8dc3")
 
-  .then (function(res) {
-    return res.json();
-  })
+    .then(function (res) {
+      return res.json();
+    })
 
-  .then (function(data) {
-    console.log(data);
-    var climateClockEl = document.querySelector('.climateClock');
-    var webTitle = (data.response.results[0].webTitle);
-    var webUrl = (data.response.results[0].webUrl);
-    var buttonEl = document.createElement("button");
-    var aEl = document.createElement("a");
-    aEl.href=webUrl;
-    aEl.innerHTML=webTitle;
-    console.log(aEl);
-    //buttonEl.textContent = aEl;
-    climateClockEl.appendChild(aEl);
-  });
+    .then(function (data) {
+      console.log(data);
+      var climateClockEl = document.querySelector('.climateClock');
+      var webTitle = (data.response.results[0].webTitle);
+      var webUrl = (data.response.results[0].webUrl);
+      var buttonEl = document.createElement("button");
+      var aEl = document.createElement("a");
+      aEl.href = webUrl;
+      aEl.innerHTML = webTitle;
+      console.log(aEl);
+      //buttonEl.textContent = aEl;
+      climateClockEl.appendChild(aEl);
+    });
 
->>>>>>> aac67c49a59f6641735fdb0972cd08385698477d
 
   output.push(
     `   <div id="questions"><h2> ${question.question} </h2></div>
@@ -492,86 +489,86 @@ submitScore.addEventListener("click", saveScore);
 var pollutionEl = $("#pollution");
 // var cityName = 'Toronto';
 var currentDate = moment().format("MM/DD/YYYY");
-var apiKey='936da452efddec94d3bf53bc5ce3701728278b67 '
+var apiKey = '936da452efddec94d3bf53bc5ce3701728278b67 '
 var airPollutionUrl = 'https://api.waqi.info/feed/'
 
 function getPollutionData(cityName) {
-fetch(airPollutionUrl + cityName + "/?token=" + apiKey)
-.then(function(response){
-    return response.json();
-})
-.then(function(pollutionData){
-    console.log("pollutionData= ", pollutionData);
-    displayPollution(pollutionData);
-})
+  fetch(airPollutionUrl + cityName + "/?token=" + apiKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (pollutionData) {
+      console.log("pollutionData= ", pollutionData);
+      displayPollution(pollutionData);
+    })
 }
-function displayPollution(pollutionData){
+function displayPollution(pollutionData) {
 
-    // clear out the previous data
-    pollutionEl.empty();
-    const cityNameEl = $("<div>");
-    const aqiEl = $("<div>");
-    const airQualityEl = $("<div>");
-    const pm25El = $("<div>");
-    const pm10El = $("<div>");
-    const uviEl = $("<div>");
-    const ozoneEl = $("<div>");
+  // clear out the previous data
+  pollutionEl.empty();
+  const cityNameEl = $("<div>");
+  const aqiEl = $("<div>");
+  const airQualityEl = $("<div>");
+  const pm25El = $("<div>");
+  const pm10El = $("<div>");
+  const uviEl = $("<div>");
+  const ozoneEl = $("<div>");
 
-    aqi = pollutionData.data.aqi;
-    pm25 = pollutionData.data.forecast.daily.pm25[2].avg;
-    pm10 = pollutionData.data.forecast.daily.pm10[2].avg;
-    uvi = pollutionData.data.forecast.daily.uvi[2].avg;
-    ozone = pollutionData.data.forecast.daily.o3[2].avg;
+  aqi = pollutionData.data.aqi;
+  pm25 = pollutionData.data.forecast.daily.pm25[2].avg;
+  pm10 = pollutionData.data.forecast.daily.pm10[2].avg;
+  uvi = pollutionData.data.forecast.daily.uvi[2].avg;
+  ozone = pollutionData.data.forecast.daily.o3[2].avg;
 
-    console.log("pm25=", pm25);
-    cityNameEl.text(cityName + " Air Quality");
-    if (aqi < 51) {
-        airQuality = "Good";
-        cityNameEl.addClass("green");
-        aqiEl.addClass("green");
-        airQualityEl.addClass("green");
-    }
-    else if (aqi > 50 && aqi < 101) {
-        airQuality = "Moderate"
-        aqiEl.addClass("yellow");
-    }
-    else if (aqi > 100 && aqi < 151){
-        airQuality = "Unhealthy";
-        aqiEl.addClass("orange");
-    }
-    else if (aqi > 150) {
-        airQuality = "Hazardous";
-        aqiEl.addClass("red");
-    }
+  console.log("pm25=", pm25);
+  cityNameEl.text(cityName + " Air Quality");
+  if (aqi < 51) {
+    airQuality = "Good";
+    cityNameEl.addClass("green");
+    aqiEl.addClass("green");
+    airQualityEl.addClass("green");
+  }
+  else if (aqi > 50 && aqi < 101) {
+    airQuality = "Moderate"
+    aqiEl.addClass("yellow");
+  }
+  else if (aqi > 100 && aqi < 151) {
+    airQuality = "Unhealthy";
+    aqiEl.addClass("orange");
+  }
+  else if (aqi > 150) {
+    airQuality = "Hazardous";
+    aqiEl.addClass("red");
+  }
 
-    // Fill the data 
-    
-    aqiEl.text(aqi);
-    airQualityEl.text(airQuality);
-    pm25El.text("PM2.5 " + pm25);
-    pm10El.text("PM10 " + pm10);
-    ozoneEl.text("O3 " + ozone);
-    uviEl.text("UVI " + uvi);
+  // Fill the data 
 
-    //  Append the elements
-    pollutionEl.append(cityNameEl);
-    pollutionEl.append(aqiEl);
-    pollutionEl.append(airQualityEl);
-    pollutionEl.append(pm25El);
-    pollutionEl.append(pm10El);
-    pollutionEl.append(ozoneEl);
-    pollutionEl.append(uviEl);
+  aqiEl.text(aqi);
+  airQualityEl.text(airQuality);
+  pm25El.text("PM2.5 " + pm25);
+  pm10El.text("PM10 " + pm10);
+  ozoneEl.text("O3 " + ozone);
+  uviEl.text("UVI " + uvi);
+
+  //  Append the elements
+  pollutionEl.append(cityNameEl);
+  pollutionEl.append(aqiEl);
+  pollutionEl.append(airQualityEl);
+  pollutionEl.append(pm25El);
+  pollutionEl.append(pm10El);
+  pollutionEl.append(ozoneEl);
+  pollutionEl.append(uviEl);
 
 }
 
 // Click the Search button
 var searchCityEl = $("#searchCity");
 
-searchCityEl.click(function() {
-    var inputEl = $("#cityName");
-    cityName = inputEl.val().toUpperCase();
-    console.log("City name is = ", cityName);
-    getPollutionData(cityName);
+searchCityEl.click(function () {
+  var inputEl = $("#cityName");
+  cityName = inputEl.val().toUpperCase();
+  console.log("City name is = ", cityName);
+  getPollutionData(cityName);
 })
 
 

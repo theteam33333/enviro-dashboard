9729a96ab85b06d951cf463a063aab03bc093a53
warNebula <colinnebula@hotmail.com>
2021-08-02 22:50:48 -0400

    Colin updated the index.html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,18 +99,19 @@
 
 // Colin -------------------------------------------------------------------------------------------//
 // THE JAVASCRIPT CODE FOR THE SEARCH INPUT FORM STARTS HERE-------------------------------------------------------------------------------------------//
-var infoEl = document.querySelector('#quiz');
-//Test the console with a function call
-var getSearchInfo = function() {
-  console.log("function was called");
-};
+$(document).ready(function(){
+  $(".default_option").click(function(){
+    $(".dropdown ul").addClass("active");
+  });
+  $(".dropdown ul li").click(function(){
+    var text = $(this).text();
+    $(".default_option").text(text);
+    $(".dropdown ul").removeClass("active");
+  });
+});
 
-getSearchInfo();
-
-
-
-
-
+     
+  
 
 
   // Colin -------------------------------------------------------------------------------------------//
